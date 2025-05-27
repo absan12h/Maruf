9ac07f59d0b25ca3f8d05eced8645ea90289dd28
fahad1234-1234 <gamingfahad092@gmail.com>
2025-05-27 11:36:33 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "🚸"){
-  if(event.userID == "61576178781718","61555745371339","100045823417357"){
+  if(event.userID == "61555745371339"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🦆"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61576178781718","61555745371339","100045823417357"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61555745371339"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
